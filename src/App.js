ec17de03dc0c4437ebc93129ee859d0ccc0dbbb2
Dibss <ludovicad01@gmail.com>
2022-08-12 23:09:17 +0200

    added components

--- src/App.js
+++ src/App.js
@@ -3,10 +3,17 @@ import React from 'react';
 import UserForm from './components/Users/UserForm';
 import NewUser from './components/Users/NewUser';
 
-function App() {
+const App = props => {
+
+  const user = {};
+
+  const addUser = () => {
+    user.push(props.newUser);
+  }
+
   return (
     <div>
-      <UserForm />
+      <UserForm onSubmit={addUser}/>
       <NewUser />
     </div>
   );
