babfd70cfe83958c9946887f7f9309fc5b05686b
Dibss <ludovicad01@gmail.com>
2022-08-12 23:09:51 +0200

    added useState and handlers

--- src/components/Users/UserForm.js
+++ src/components/Users/UserForm.js
@@ -1,18 +1,42 @@
-import Button from "../UI/Button";
+import React, {useState} from "react";
+
 import styles from "./css/UserForm.module.css";
 
 const UserForm = props => {
+  const [enteredUsername, setEnteredUsername] = useState('');
+  const [enteredAge, setEnteredAge] = useState('');
+
+  const usernameHandler = event => {
+    setEnteredUsername(event.target.value);
+  };
+
+  const ageHandler = event => {
+    setEnteredAge(event.target.value);
+  }
+
+  const addUserHandler = event => {
+
+    event.preventDefault();
+
+    const newUser = {
+      username: enteredUsername,
+      age: enteredAge,
+    }
+
+    props.onSubmit(newUser);
+  }
+
   return (
-    <form className={styles.input}>
+    <form className={styles.input} onSubmit={addUserHandler}>
       <div>
-        <label>User</label>
-        <input type="text"/>
+        <label htmlFor="">Username</label>
+        <input type="text" id="username" onChange={usernameHandler}/>
       </div>
       <div>
-        <label>Age</label>
-        <input type="number"/>
+        <label htmlFor="age">Age (Years)</label>
+        <input type="number" id="age" onChange={ageHandler}/>
       </div>
-      <Button />
+      <button type="submit">Add user</button>
     </form>
   );
 };
