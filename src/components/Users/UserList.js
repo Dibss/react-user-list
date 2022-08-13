8e0e715354cf2a040a0bb96c4cd80df8595b1af3
Dibss <ludovicad01@gmail.com>
2022-08-13 16:27:56 +0200

    added map function and case with 0 elements in arr

--- src/components/Users/UserList.js
+++ src/components/Users/UserList.js
@@ -1,9 +1,29 @@
-const UserList = props => {
-  return(
-    <div>
-      
-    </div>
+import Card from "../UI/Card";
+import styles from "./css/UserList.module.css";
+
+const UserList = (props) => {
+
+  const userList = props.list;
+
+  let ulContent = (
+    userList.map((user) => (
+      <li key={user.id}>
+        {user.username} ({user.age} years old)
+      </li>
+    ))
   )
-}
+
+  if(userList.length === 0){
+    ulContent = 'Non ci sono users registrati'
+  }
+
+  return (
+    <Card className={styles.users}>
+      <ul>
+        {ulContent}
+      </ul>
+    </Card>
+  );
+};
 
 export default UserList;
